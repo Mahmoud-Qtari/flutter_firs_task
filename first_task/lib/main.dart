2316f49c769bf17dde2bf5081e20dfd588be8174
Mahmoud-Qtari <66205809+Mahmoud-Qtari@users.noreply.github.com>
2021-03-27 15:38:05 +0300

    Update main.dart

--- first_task/lib/main.dart
+++ first_task/lib/main.dart
@@ -1,11 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'CounterView.dart';
-import 'SubPage.dart';
-import 'package:flutter/material.dart';
 
-import 'counter_cubit.dart';
+import 'MainPage.dart';
 
 void main() => runApp(App());
 
@@ -13,36 +9,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Navigation',
       home: MainPage(),
     );
-  }
-}
-
-class MainPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Counter App'),
-      ),
-
-      body: BlocProvider(
-        create: (context) => CounterCubit(),
-        child: CounterView(),
-      ),
-
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.arrow_forward),
-        onPressed: () {
-          navigateToSubPage(context);
-        },
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-    );
-  }
-
-  Future navigateToSubPage(context) async {
-    Navigator.push(context, MaterialPageRoute(builder: (context) => SubPage()));
   }
 }
