4d806f69390a33d21b58ad15cf70fa40257a1ae3
Mahmoud-Qtari <66205809+Mahmoud-Qtari@users.noreply.github.com>
2021-04-03 12:55:46 +0300

    Update CounterView.dart

--- first_task/lib/CounterView.dart
+++ first_task/lib/CounterView.dart
@@ -1,12 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'counter_cubit.dart';
-import 'package:bloc/bloc.dart';
 class CounterView extends StatefulWidget {
   @override
   _CounterViewState createState() => _CounterViewState();
 }
-int count = counter;
 class _CounterViewState extends State<CounterView> {
   @override
   Widget build(BuildContext context) {
@@ -18,16 +16,14 @@ class _CounterViewState extends State<CounterView> {
           children: <Widget>[
             IconButton(icon: Icon(Icons.remove), onPressed: (){
               context.read<CounterCubit>().decrement();
-              
             }),
             //execute when state changes
             BlocBuilder<CounterCubit, int>(
-              builder: (context, state) {
+              builder: (context,state){
                 return Text('$state', style: textTheme.headline2);
               },
             ),
             IconButton(icon: Icon(Icons.add), onPressed: (){
-              //print(stateCount);
               context.read<CounterCubit>().increment();
             })
           ],
